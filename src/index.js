9ece8d577ed897eabf56c03f71d3b08f2cf0c8b0
Hichami Houssam <hichami.houssam@gmail.com>
2021-10-12 11:42:19 +0100

    api functionnality to retrive and populate html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import './style.css';
-import { fetchMealData } from './api.js';
+import { fetchMealData, fetchMeals } from './api.js';
+const mealIds = [52768, 52776, 52765, 52935, 52960, 52962];
 
 const populate = async () => {
-  const data = await fetchMealData();
-  const container = document.querySelector('.container');
-  const card = document.createElement('div');
+  //const data = await fetchMealData();
 
-  card.className = 'card';
-  console.log(data[0]);
-  card.innerHTML = `${data[0].strMeal} ${data[0].strMealThumb}`;
-  container.appendChild(card);
+  const data = [];
+
+  await mealIds.forEach(id => data.push(fetchMeals(id)))
+  //console.log(data);
+  data.forEach((data) => (data.then((meal) => {
+      //console.log(meal.meals[0])
+
+      const card = document.createElement('div');
+
+    card.className = 'card';
+    //console.log(data.all());
+    card.innerHTML = `  <span>${meal.meals[0].strMeal}</span> <img src="${meal.meals[0].strMealThumb}" alt="logo">`;
+    container.appendChild(card);
+  })))
+    
+//  const data = await fetchMeals()
+  const container = document.querySelector('.container');
+  
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
