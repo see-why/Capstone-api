10d096a6305ab48ee6468b214ea31319f21ee33d
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-12 14:09:37 +0200

    Add api functionality and push meals to local storage

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 import './style.css';
-import { fetchMealData, fetchMeals } from './api.js';
+import { fetchMeals } from './api.js';
 const mealIds = [52768, 52776, 52765, 52935, 52960, 52962];
 
 const populate = async () => {
-  //const data = await fetchMealData();
-
   const data = [];
 
-  await mealIds.forEach(id => data.push(fetchMeals(id)))
-  //console.log(data);
-  data.forEach((data) => (data.then((meal) => {
-      //console.log(meal.meals[0])
+  const mealArray = [];
 
-      const card = document.createElement('div');
+  await mealIds.forEach(id => data.push(fetchMeals(id)))
+  data.forEach((data) => (data.then((meal) => {
+    const card = document.createElement('div');
 
     card.className = 'card';
-    //console.log(data.all());
-    card.innerHTML = `  <span>${meal.meals[0].strMeal}</span> <img src="${meal.meals[0].strMealThumb}" alt="logo">`;
+    card.innerHTML = `<div class="card">
+                        <img src="${meal.meals[0].strMealThumb}
+                        " alt="meal" class="foodPic">
+                        <div class="like">
+                          <h3>${meal.meals[0].strMeal}</h3>
+                          <a href="#"><i class="fas fa-heart"></i></a>
+                        </div>
+                        <h4 class="likesNum"></h4>
+                        <button class="comment"><i class="fas fa-comment"></i>  Comment</button>
+                        <button class="Reservaton">Reservaton <i class="fa fa-pencil" aria-hidden="true"></i></button>
+                      </div>`;
     container.appendChild(card);
-  })))
-    
-//  const data = await fetchMeals()
+
+    mealArray.push(meal.meals);
+    localStorage.setItem('Meals', JSON.stringify(mealArray));
+  })));
+
   const container = document.querySelector('.container');
-  
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
