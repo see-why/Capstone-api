93b66ea2ca646bc5fc6dd3c2f7c7e41b2ccf766a
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-12 11:48:57 +0200

    get data from API

--- src/api.js
+++ src/api.js
@@ -1,14 +1,16 @@
-const mealIds = [52768,52776,52765,52935,52960,52962];
+const baseUrl = 'http://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 
-export const fetchMealData = async () => {
-    let responseArr = [];
+const mealIds = [52768, 52776, 52765, 52935, 52960, 52962];
 
-    mealIds.forEach(async (mealId) => {
-        const response = await fetch(`${baseUrl}${mealId}`); 
-        const mealData = await response.json();
-        responseArr.push(mealData);    
+const fetchMealData = async () => {
+  let responseArr = [];
 
-    })
-    
-    console.log(responseArr);
-  };
+  mealIds.forEach(async (mealId) => {
+    const response = await fetch(`${baseUrl}${mealId}`);
+    const mealData = await response.json();
+    responseArr.push(mealData.meals);
+  });
+  return responseArr;
+};
+
+export { fetchMealData };
