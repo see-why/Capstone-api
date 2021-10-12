b29ca466d72f78c31c80cfe45485668899ad45a0
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-12 11:49:17 +0200

    Populate html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import './style.css';
+import { fetchMealData } from './api.js';
 
-const baseUrl = 'http://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+const populate = async () => {
+  const data = await fetchMealData();
+  const container = document.querySelector('.container');
+  const card = document.createElement('div');
 
- fetchMealData();
+  card.className = 'card';
+  console.log(data[0]);
+  card.innerHTML = `${data[0].strMeal} ${data[0].strMealThumb}`;
+  container.appendChild(card);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await populate();
+});
