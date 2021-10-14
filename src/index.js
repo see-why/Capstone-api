62af398c89ded4978e9833c74333ab2c1c298dd8
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-14 20:54:04 +0200

    Implement likes counter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { createComments } from './comments_api.js';
 import {
   body, container, popup, popupComment, populateComments,
 } from './commentEvent.js';
-import { getLikes } from './likes.js';
+import { getLikes, updateLikes } from './likes.js';
 
 const mealIds = [52768, 52776, 52765, 52935, 52960, 52962];
 
@@ -17,28 +17,30 @@ const populate = async () => {
 
   const mealArray = [];
 
-  const dataLikes = await getLikes();
+  let dataLikes = await getLikes();
+  dataLikes = dataLikes.filter((like) => typeof (like.item_id) == 'number');
 
   mealIds.forEach((id) => data.push(fetchMeals(id)));
   data.forEach((data) => (data.then((meal) => {
     const card = document.createElement('div');
-    const likesArray = dataLikes.filter((like) => like.item_id.toString() === meal.meals[0].idMeal);
+    const likesArray = dataLikes.filter(
+      (like) => like.item_id == parseInt(meal.meals[0].idMeal, 10),
+    );
     card.className = 'card';
     card.innerHTML = `<div class="card" id="${meal.meals[0].idMeal}">
                         <img src="${meal.meals[0].strMealThumb}
                         " alt="meal" class="foodPic">
                         <div class="like">
                           <h3>${meal.meals[0].strMeal}</h3>
-                          <a href="#"><i class="fas fa-heart"></i></a>
+                          <a href="#"><i class="fas fa-heart give-like" id="${meal.meals[0].idMeal}"></i></a>
+                          <h4 class="likesNum">${likesArray[0].likes + 1} likes</h4>
                         </div>
-                        <h4 class="likesNum">${likesArray[0].likes} likes</h4>
                         <button id = "${meal.meals[0].idMeal}" class="comment"><i class="fas fa-comment"></i>  Comment</button>
                         <button class="Reservaton">Reservaton <i class="fa fa-pencil" aria-hidden="true"></i></button>
                       </div>`;
     container.appendChild(card);
     mealArray.push(meal.meals);
     addReservationButtonEvent();
-
     localStorage.setItem('Meals', JSON.stringify(mealArray));
   })));
 
@@ -48,6 +50,12 @@ const populate = async () => {
 
 document.addEventListener('DOMContentLoaded', async () => {
   await populate();
+});
+
+body.addEventListener('click', async (e) => {
+  if (e.target && e.target.matches('.give-like')) {
+    updateLikes(e.target);
+  }
 });
 
 container.addEventListener('click', (e) => {
