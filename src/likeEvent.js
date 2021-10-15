871b7b0391c77ee3c26e81d090a5b83135c1ae21
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-15 15:08:47 +0200

    Test the like counter function

---
+++ src/likeEvent.js
@@ -0,0 +1,15 @@
+import { getLikes, createLikes } from './likes.js';
+
+const updateLikes = async (eventTarget) => {
+  await createLikes(parseInt(eventTarget.id, 10));
+  let dataLikes = await getLikes();
+  /* eslint-disable */
+  dataLikes = dataLikes.filter((like) => typeof (like.item_id) == 'number');
+  const likesArray = dataLikes.filter((like) => like.item_id == parseInt(eventTarget.id, 10));
+  /* eslint-enable */
+  let count = likesArray[0].likes;
+  count += 1;
+  eventTarget.parentNode.parentNode.lastChild.previousSibling.innerHTML = `${count} likes`;
+};
+
+export default updateLikes;
